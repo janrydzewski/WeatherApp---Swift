6f5f18dd0be98701ac3b34dc8ef4c713d6c4cd77
j_rydzewski <52713164+janrydzewski@users.noreply.github.com>
2024-05-07 08:29:55 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Jan Rydzewski on 07/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
